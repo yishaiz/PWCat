1a4dad5538d3d5e1f6a40be8438e275998021f16
z7\yishai <yishai5555@gmail.com>
2018-01-09 15:37:34 +0200

    b6

--- src/app/img-card/img-card.component.ts
+++ src/app/img-card/img-card.component.ts
@@ -18,7 +18,21 @@ import { CatImage } from "../cat-image";
                  alt="Cute cat"
                  mat-card-image>
         </mat-card>  `,
-    styles : []
+    styles : [`
+
+        .mat-card {
+            width: 400px;
+            margin: 2rem auto;
+        }
+        .mat-card .mat-card-actions {
+            padding-top: 0;
+        }
+        .mat-card .mat-button {
+            margin: 0 auto;
+            display: block;
+        }
+        
+    `]
 })
 export class ImgCardComponent implements OnInit {
     public src : string;
@@ -38,7 +52,8 @@ export class ImgCardComponent implements OnInit {
 
     generateSrc () : void {
         this.src = this.image.api + this.image.message +
-                   '?size=' + this.image.fontsize;
+                   '?size=' + this.image.fontsize +
+                   '&ts=' + Date.now();
     }
 
 
