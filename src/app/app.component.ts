45646206b84cf15e2a05b56bf0354c7c11e8b833
z7\yishai <yishai5555@gmail.com>
2018-01-09 10:47:54 +0200

    b2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-         {{title}} 
-      </h1>
- </div>    
-  `,
-  styles: []
+    selector : 'app-root',
+    template : `
+        <!--The content below is only a placeholder and can be replaced.-->
+        <div style="text-align:center">
+            <mat-toolbar color="primary">
+                {{ title }}
+            </mat-toolbar>
+        </div>
+    `,
+    styles : []
 })
 export class AppComponent {
     title = 'Progressive Web Cat';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations : [
+        AppComponent
+    ],
+    imports : [
+        BrowserModule,
+        MatToolbarModule
+    ],
+    providers : [],
+    bootstrap : [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
