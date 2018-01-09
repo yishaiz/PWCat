216b7eee1598612a3b95cb296e7249325a82601f
z7\yishai <yishai5555@gmail.com>
2018-01-09 15:27:59 +0200

    b4

--- src/app/img-card/img-card.component.ts
+++ src/app/img-card/img-card.component.ts
@@ -1,21 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { CatImage } from "../cat-image";
 
 @Component({
     selector : 'app-img-card',
     template : `
         <mat-card>
-            <img src="https://cataas.com/cat/says/Progressive%20Web%20Cat"
+            <img src="{{src}}"
                  alt="Cute cat"
                  mat-card-image>
         </mat-card>  `,
     styles : []
 })
 export class ImgCardComponent implements OnInit {
+    public src : string;
+
+    private image : CatImage = {
+        message : 'Progressive Web Cat',
+        api : 'https://cataas.com/cat/says/',
+        fontsize : 40
+    };
 
     constructor () {
     }
 
     ngOnInit () {
+        this.generateSrc();
     }
+
+    generateSrc () : void {
+        this.src = this.image.api + this.image.message +
+                   '?size=' + this.image.fontsize;
+    }
+
 
 }
